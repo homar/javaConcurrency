2da1438654e45ab1c782e601e1e798e7846539ef
homar <konraddziedzic@gmail.com>
2013-04-06 00:05:19 +0100

    using local threads variable

---
+++ src/pl/homar/chapter10/Result.java
@@ -0,0 +1,13 @@
+package pl.homar.chapter10;
+
+public class Result {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
