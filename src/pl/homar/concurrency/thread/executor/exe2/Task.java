6047b58e8daedbac36246b689b1001f1ed6d3941
homar <konraddziedzic@gmail.com>
2013-04-12 22:54:19 +0200

    fixed sized thread executor

---
+++ src/pl/homar/concurrency/thread/executor/exe2/Task.java
@@ -0,0 +1,31 @@
+package pl.homar.concurrency.thread.executor.exe2;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Task implements Runnable {
+
+	private Date initDate;
+	private String name;
+	
+	public Task(String name) {
+		super();
+		this.initDate = new Date();
+		this.name = name;
+	}
+
+	@Override
+	public void run() {
+		System.out.printf("%s: Task %s: Created on: %s \n", Thread.currentThread().getName(), name, initDate);
+		System.out.printf("%s: Task %s: Started on: %s\n", Thread.currentThread().getName(), name, new Date());
+		try{
+			long duration = (long)(Math.random()*10);
+			System.out.printf("%s Task %s: Doing a task during %d seconds\n", Thread.currentThread().getName(), name, duration);
+			TimeUnit.SECONDS.sleep(duration);
+		}catch(InterruptedException e){
+			e.printStackTrace();
+		}
+		System.out.printf("%s: Task %s: Finished on: %s\n", Thread.currentThread().getName(), name, new Date());
+	}
+
+}
